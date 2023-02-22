3ff2d7b7573fca6f3b28f94228842da0d11289af
AbdulWasey2211002TRACND <119413589+AbdulWasey2211002TRACND@users.noreply.github.com>
2023-02-23 00:24:04 +0500

    test cases added
    test cases added

--- src/main/java/com/example/test/Controllers/Routes.java
+++ src/main/java/com/example/test/Controllers/Routes.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -20,25 +22,26 @@ public class Routes {
     @Autowired
     private OrdersRepository order;
 
-   
     @PostMapping("/add_orders")
-    public ResponseEntity Orders(@RequestBody Orders model) 
-    {
+    public ResponseEntity Orders(@RequestBody Orders model) {
 
         order.save(model);
-        return new ResponseEntity<Object>("Order Added Successfully" ,HttpStatus.OK);    
+        return new ResponseEntity<Object>("Order Added Successfully", HttpStatus.OK);
 
-        }
+    }
 
     @GetMapping("/get_orders")
-    public ResponseEntity GetOrders() 
-    {
+    public ResponseEntity GetOrders() {
 
-        return new ResponseEntity<Object>(order.findAll(),HttpStatus.OK);    
+        return new ResponseEntity<Object>(order.findAll(), HttpStatus.OK);
 
-        }
+    }
 
-    
+    @DeleteMapping("/delete_order")
+    public ResponseEntity DeleteOrder(@RequestParam Long id) {
+        order.deleteById(id);
+        return new ResponseEntity<Object>("Order Deleted Successfully", HttpStatus.OK);
 
-   
+    }
+
 }
--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.test;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.test.Controllers.Routes;
 import com.example.test.Models.Orders;
+import com.example.test.Repositories.OrdersRepository;
 
 @SpringBootTest
 class TestApplicationTests {
+
+	@Mock
+	private OrdersRepository orderrepo;
+
+	@InjectMocks
+	private Routes routesController;
 
 	@Test
 	public void testConstructor() {
@@ -38,6 +54,42 @@ class TestApplicationTests {
 		assertEquals(5L, order.getQuantity());
 		assertEquals(2L, order.getProductid());
 		assertEquals("imagelink", order.getImage_link());
+	}
+
+	@Test
+	public void testAddOrder() {
+		Orders order = new Orders();
+		order.setName("Test Order");
+		order.setQuantity((long) 5);
+		order.setPrice((long) 10.0);
+		ResponseEntity response = routesController.Orders(order);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertTrue(response.getBody().toString().contains("Order Added Successfully"));
+	}
+
+	@Test
+	public void testGetOrders() {
+		ResponseEntity response = routesController.GetOrders();
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+	}
+
+	@Test
+	public void testDeleteOrder() {
+		Orders order = new Orders();
+		order.setName("Test Order");
+		order.setQuantity((long) 5);
+		order.setPrice((long) 10.0);
+		orderrepo.save(order);
+
+		ResponseEntity response = routesController.DeleteOrder(order.getId());
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+
+		assertTrue(response.getBody().toString().contains("Order Deleted Successfully"));
+		assertFalse(orderrepo.findById(order.getId()).isPresent());
 	}
 
 }
