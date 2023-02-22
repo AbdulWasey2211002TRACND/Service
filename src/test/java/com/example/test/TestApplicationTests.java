1be82f2cae8848596796312bf1d5787134a0042e
AbdulWasey2211002TRACND <119413589+AbdulWasey2211002TRACND@users.noreply.github.com>
2023-02-22 23:54:14 +0500

    test case added
    test case added

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -2,12 +2,42 @@ package com.example.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.test.Models.Orders;
 
 @SpringBootTest
 class TestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testConstructor() {
+		Orders order = new Orders(1L, "Shirt", 100L, 500L, 5L, 2L, "imagelink");
+		assertEquals(1L, order.getId());
+		assertEquals("Shirt", order.getName());
+		assertEquals(100L, order.getPrice());
+		assertEquals(500L, order.getTotalprice());
+		assertEquals(5L, order.getQuantity());
+		assertEquals(2L, order.getProductid());
+		assertEquals("imagelink", order.getImage_link());
+	}
+
+	@Test
+	public void testGettersAndSetters() {
+		Orders order = new Orders();
+		order.setId(1L);
+		order.setName("Shirt");
+		order.setPrice(100L);
+		order.setTotalprice(500L);
+		order.setQuantity(5L);
+		order.setProductid(2L);
+		order.setImage_link("imagelink");
+		assertEquals(1L, order.getId());
+		assertEquals("Shirt", order.getName());
+		assertEquals(100L, order.getPrice());
+		assertEquals(500L, order.getTotalprice());
+		assertEquals(5L, order.getQuantity());
+		assertEquals(2L, order.getProductid());
+		assertEquals("imagelink", order.getImage_link());
 	}
 
 }
